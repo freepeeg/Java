7e00e5269b7937f938d996d8f4859858a30afdfe
Ian <iancaffey@ymail.com>
2014-06-06 00:12:22 -0500

    Removed generic parameter to allow implementation

--- src/iitc/projects/bot/BFrame.java
+++ src/iitc/projects/bot/BFrame.java
@@ -12,16 +12,16 @@ import java.util.Collections;
  * @author Ian
  * @version 1.0
  */
-public class BFrame<B extends BPanel, B2 extends Manager> extends JInternalFrame {
-    private final java.util.List<LoadListener<B, B2, BFrame<B, B2>>> listeners;
-    private final B instance;
+public class BFrame<B2 extends Manager> extends JInternalFrame {
+    private final java.util.List<LoadListener<BPanel, B2, BFrame<B2>>> listeners;
+    private final BPanel instance;
     private B2 manager;
 
-    public BFrame(B instance) {
+    public BFrame(BPanel instance) {
         this("", instance);
     }
 
-    public BFrame(String title, B instance) {
+    public BFrame(String title, BPanel instance) {
         super(title, true, true, true, true);
         this.listeners = new ArrayList<>();
         this.instance = instance;
@@ -37,13 +37,13 @@ public class BFrame<B extends BPanel, B2 extends Manager> extends JInternalFrame
     }
 
     @SafeVarargs
-    public final void listen(LoadListener<B, B2, BFrame<B, B2>>... listeners) {
+    public final void listen(LoadListener<BPanel, B2, BFrame<B2>>... listeners) {
         Collections.addAll(this.listeners, listeners);
     }
 
     public void load() {
         instance.load(this);
-        for (LoadListener<B, B2, BFrame<B, B2>> listener : listeners)
+        for (LoadListener<BPanel, B2, BFrame<B2>> listener : listeners)
             listener.onLoad(this);
     }
 
--- src/iitc/projects/bot/LoadListener.java
+++ src/iitc/projects/bot/LoadListener.java
@@ -10,6 +10,6 @@ import java.util.EventListener;
  * @author Ian
  * @version 1.0
  */
-public interface LoadListener<B1 extends BPanel, B2 extends Manager, B3 extends BFrame<B1, B2>> extends EventListener {
+public interface LoadListener<B1 extends BPanel, B2 extends Manager, B3 extends BFrame<B2>> extends EventListener {
     public void onLoad(B3 parent);
 }
